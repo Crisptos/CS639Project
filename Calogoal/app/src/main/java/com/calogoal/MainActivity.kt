2f011cc774487eaeb83a83f26030e9704ba2434c
Christos Mark <baratheoniron@gmail.com>
2025-11-24 21:28:58 -0500

    Added a basic nav host with the meal tracker screen. Added a MealTracker screen and an enum/data class to represent food being displayed.

--- Calogoal/app/src/main/java/com/calogoal/MainActivity.kt
+++ Calogoal/app/src/main/java/com/calogoal/MainActivity.kt
@@ -1,47 +1,31 @@
 package com.calogoal
-
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.calogoal.ui.theme.CalogoalTheme
+import androidx.compose.runtime.*
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            CalogoalTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            Calogoal()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun Calogoal() {
+    val navController = rememberNavController()
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CalogoalTheme {
-        Greeting("Android")
+    NavHost(
+        navController = navController,
+        startDestination = "MealTracking"
+    ) {
+        composable("MealTracking") { MealTracker(navController) }
     }
 }
---
+++ Calogoal/app/src/main/java/com/calogoal/models/TrackedFood.kt
@@ -0,0 +1,9 @@
+package com.calogoal.models
+
+data class TrackedFood(
+    val label: String,
+    val calories: Int,
+    val protein: Int,
+    val fat: Int,
+    val carbs: Int
+)
