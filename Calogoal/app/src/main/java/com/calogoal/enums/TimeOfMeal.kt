468ebf875fde91c10b9b4d32f497752c5a756765
Mohammed <naziryusif8@gmail.com>
2025-12-11 23:44:21 -0500

    Resolved references in enums.TimeOfMeal

--- Calogoal/app/src/main/java/com/calogoal/enums/TimeOfMeal.kt
+++ Calogoal/app/src/main/java/com/calogoal/enums/TimeOfMeal.kt
@@ -1,15 +1,15 @@
 package com.calogoal.enums
 
 enum class TimeOfMeal(val value: Int) {
-    BREAKFAST(0),
-    LUNCH(1),
-    DINNER(2),
-    SNACK(3),
-    OTHER(99); // Use a distinct value for uncategorized/other
+    Breakfast(0),
+    Lunch(1),
+    Dinner(2),
+    Snack(3),
+    Other(99); // Use a distinct value for uncategorized/other
 
     // Companion object to help convert the integer value from Firestore back into the Enum
     companion object {
         private val map = entries.associateBy(TimeOfMeal::value)
-        fun fromInt(type: Int) = map[type] ?: OTHER
+        fun fromInt(type: Int) = map[type] ?: TimeOfMeal.Other
     }
 }
